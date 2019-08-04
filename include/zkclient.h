80d299a3c70d78647c845534add2865a61cc152f
Shuai Zhang <zhangshuai.ustc@gmail.com>
2019-08-04 21:36:57 +0800

    Add ZClient

--- include/zkclient.h
+++ include/zkclient.h
@@ -1,2 +1,49 @@
 #pragma once
 
+namespace hcoona {
+namespace zookeeper {
+
+enum class ZErrorCode {
+  ZOK = 0,
+  ZSYSTEMERROR = -1,
+  ZRUNTIMEINCONSISTENCY = -2,
+  ZDATAINCONSISTENCY = -3,
+  ZCONNECTIONLOSS = -4,
+  ZMARSHALLINGERROR = -5,
+  ZUNIMPLEMENTED = -6,
+  ZOPERATIONTIMEOUT = -7,
+  ZBADARGUMENTS = -8,
+  ZINVALIDSTATE = -9,
+  ZAPIERROR = -100,
+  ZNONODE = -101,
+  ZNOAUTH = -102,
+  ZBADVERSION = -103,
+  ZNOCHILDRENFOREPHEMERALS = -108,
+  ZNODEEXISTS = -110,
+  ZNOTEMPTY = -111,
+  ZSESSIONEXPIRED = -112,
+  ZINVALIDCALLBACK = -113,
+  ZINVALIDACL = -114,
+  ZAUTHFAILED = -115,
+  ZCLOSING = -116,
+  ZNOTHING = -117,
+  ZSESSIONMOVED = -118
+};
+
+class ZClient {
+ public:
+  ZClient();
+  ZClient(const ZClient&) = delete;
+  ZClient& operator=(const ZClient&) = delete;
+  ZClient(ZClient&&);
+  ZClient& operator=(ZClient&& other);
+  virtual ~ZClient();
+
+ private:
+  void Close();
+
+  void* handle_;
+};
+
+}  // namespace zookeeper
+}  // namespace hcoona
--- src/zkclient.cc
+++ src/zkclient.cc
@@ -1,1 +1,37 @@
 #include "zkclient.h"
+
+#include <utility>
+
+#include "zookeeper.h"
+
+namespace hcoona {
+namespace zookeeper {
+
+ZClient::ZClient() = default;
+
+ZClient::ZClient(ZClient&& other) {
+  handle_ = other.handle_;
+  other.handle_ = nullptr;
+}
+
+ZClient& ZClient::operator=(ZClient&& other) {
+  if (this != &other) {
+    Close();
+
+    handle_ = other.handle_;
+    other.handle_ = nullptr;
+  }
+  return *this;
+}
+
+ZClient::~ZClient() { Close(); }
+
+void ZClient::Close() {
+  if (handle_) {
+    ZErrorCode error_code = static_cast<ZErrorCode>(
+        zookeeper_close(reinterpret_cast<zhandle_t*>(handle_)));
+  }
+}
+
+}  // namespace zookeeper
+}  // namespace hcoona
